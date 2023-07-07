f0038a52b32e1a187d701eda057443745fe4eeb0
KEEWOONG NAM <keewoong.nam@flicspy.com>
2023-07-08 07:13:57 +0900

    Delete google

--- modules/ecs_fargate_service/main.tf
+++ modules/ecs_fargate_service/main.tf
@@ -4,10 +4,6 @@ terraform {
       source  = "hashicorp/aws"
       version = ">= 4.59"
     }
-    google = {
-      source  = "hashicorp/google"
-      version = "> 4.10.0"
-    }
   }
 
   required_version = ">= 1.3.0"
