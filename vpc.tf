1660cf33cca2337853f87377865cc09ae1c4252a
KEEWOONG NAM <keewoong.nam@flicspy.com>
2023-10-11 08:26:01 +0900

    Clean

--- vpc.tf
+++ vpc.tf
@@ -1,15 +1,3 @@
-data "aws_vpc" "main" {
-  tags = {
-    Name = "f-development"
-  }
-}
-
-data "aws_subnet" "public_1" {
-  tags = {
-    Name = "f-development-public-1"
-  }
-}
-
 resource "aws_vpc" "main" {
   cidr_block           = local.vpc_cidr
   enable_dns_support   = true
